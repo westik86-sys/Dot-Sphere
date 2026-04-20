e999ffd96817379f8022781c17025aefaf83a449
e <e>
2026-04-20 15:37:02 +0300

    Initial Commit

---
+++ Dot-Sphere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dot-Sphere
+//
+//  Created by Pavel Korostelev on 20.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dot-Sphere/Dot_SphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dot_SphereApp.swift
+//  Dot-Sphere
+//
+//  Created by Pavel Korostelev on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Dot_SphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
